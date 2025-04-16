41023c246c0f7c03088284a4f20ecba0a7b30691
jtraveler <matthew.jtraveler@gmail.com>
2025-04-16 13:08:09 +0200

    revised the room structure to be easily read. added descriptions for the JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,93 @@
-// getters
-const narrator = document.getElementById('narrator');
+// DOM element references to connect the JS to the HTML elements
+const narrator = document.getElementById('narrator'); // The text description area
 const north = document.getElementById('north');
 const south = document.getElementById('south');
 const west = document.getElementById('west');
 const east = document.getElementById('east');
-// what we need is a array of areas/room
-// each room has a id, a name, a description, exits,
+
+// We need an array of areas/rooms
+// Room data - each one has an id, a name, a description, exits, media/visuals
+// If an exit has -1, then that means that it's inactive
 const rooms = [
     {
-        id: 0,
-        name: "cell",
+        id: 0, // Unique ID for this room
+        name: "cell", // Name of the room
         description: "you wake in a cell there is a ....way out are east, north and west",
-        west: 2,
-        east: 1,
         north: 6,
         south: -1,
+        east: 1,
+        west: 2,
+        
     },
     {
-        id: 1,
-        name: "kitchen",
+        id: 1, // Unique ID for this room
+        name: "kitchen", // Name of the room
         description: "you wake in a kitchen there is a ....",
-        west: 0,
-        east: -1,
         north: -1,
         south: -1,
+        east: -1,
+        west: 0,
+        
     },
     {
-        id: 2,
-        name: "cell",
+        id: 2, // Unique ID for this room
+        name: "car", // Name of the room
         description: "you wake in a cell there is a ....",
-        west: 3,
-        east: 0,
         north: 4,
         south: -1,
+        east: 0,
+        west: 3,
+        
+    },
+    {
+      id: 2, // Unique ID for this room
+      name: "cell", // Name of the room
+      description: "you wake in a cell there is a ....",
+      north: 4,
+      south: -1,
+      east: 0,
+      west: 3,
+      
+    },
+    {
+      id: 3, // Unique ID for this room
+      name: "cell", // Name of the room
+      description: "you wake in a cell there is a ....",
+      north: 4,
+      south: -1,
+      east: 0,
+      west: 3,
+      
+    },
+    {
+      id: 4, // Unique ID for this room
+      name: "cell", // Name of the room
+      description: "you wake in a cell there is a ....",
+      north: 4,
+      south: -1,
+      east: 0,
+      west: 3,
+      
+    },
+    {
+      id: 5, // Unique ID for this room
+      name: "cell", // Name of the room
+      description: "you wake in a cell there is a ....",
+      north: 4,
+      south: -1,
+      east: 0,
+      west: 3,
+      
+    },
+    {
+      id: 6, // Unique ID for this room
+      name: "cell", // Name of the room
+      description: "you wake in a cell there is a ....",
+      north: 4,
+      south: -1,
+      east: 0,
+      west: 3,
+      
     },
 ]
 
@@ -42,9 +97,14 @@ let currentRoom = 0;
  * This function handles the game flow
  */
 function gameMaster() {
+    // Display the current room description
     readDescrption(currentRoom);
+
+    // Set up the available exits/buttons
     handleExits(currentRoom);
 }
+
+// This function updates the narrator text
 function readDescrption(currentRoom) {
     narrator.innerText = rooms[currentRoom].description;
 }
